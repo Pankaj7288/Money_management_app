c355beb1120a7bca5ffd621666309ffab00d77d8
Pankaj Bepari <beparipankaj@gmail.com>
2020-07-11 09:05:04 +0600

    Create Users Route

---
+++ routers/userRoute.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+//Registration route
+router.post('/register', (req, res) => {
+
+})
+
+//Login route
+router.post('/login', (req, res) => {
+    
+})
+
+module.exports = router
